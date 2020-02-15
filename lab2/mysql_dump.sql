338972ac8ba577a62578e9a9c5decc14f828c7ad
Oleksii Havryshkiv <oleksii.havryshkiv@agiliway.com>
2020-02-15 14:21:27 +0200

    Add lab2.

---
+++ lab2/mysql_dump.sql
@@ -0,0 +1,279 @@
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: docker_example
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.41-MariaDB-1~bionic
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `issue`
+--
+
+DROP TABLE IF EXISTS `issue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `issue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `creator_id` int(11) DEFAULT NULL,
+  `description` mediumtext,
+  `assign_to` int(11) DEFAULT NULL,
+  `created_date` int(11) DEFAULT NULL,
+  `last_modify_date` int(11) DEFAULT NULL,
+  `priority_id` int(11) DEFAULT NULL,
+  `status_id` int(11) DEFAULT NULL,
+  `project_version_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_issue_1_idx` (`creator_id`),
+  KEY `fk_assign_to_id_idx` (`assign_to`),
+  KEY `fk_project_version_id_idx` (`project_version_id`),
+  KEY `fk_issue_status_id_idx` (`status_id`),
+  KEY `fk_issue_priority_id_idx` (`priority_id`),
+  CONSTRAINT `fk_issue_assign_to_id` FOREIGN KEY (`assign_to`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_issue_creator_id` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_issue_priority_id` FOREIGN KEY (`priority_id`) REFERENCES `issue_priority` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_issue_project_version_id` FOREIGN KEY (`project_version_id`) REFERENCES `project_version` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_issue_status_id` FOREIGN KEY (`status_id`) REFERENCES `issue_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `issue`
+--
+
+LOCK TABLES `issue` WRITE;
+/*!40000 ALTER TABLE `issue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `issue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `issue_activity`
+--
+
+DROP TABLE IF EXISTS `issue_activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `issue_activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `issue_id` int(11) DEFAULT NULL,
+  `creator_id` int(11) DEFAULT NULL,
+  `assign_to` int(11) DEFAULT NULL,
+  `description` mediumtext,
+  `status_id` int(11) DEFAULT NULL,
+  `project_version_id` int(11) DEFAULT NULL,
+  `created_date` int(11) DEFAULT NULL,
+  `priority_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_issue_id_idx` (`issue_id`),
+  KEY `fk_creator_id_idx` (`creator_id`),
+  KEY `fk_project_version_id_idx` (`project_version_id`),
+  KEY `fk_assign_to_idx` (`assign_to`),
+  KEY `fk_activity_status_id_idx` (`status_id`),
+  KEY `fk_activity_priority_id_idx` (`priority_id`),
+  CONSTRAINT `fk_activity_assign_to` FOREIGN KEY (`assign_to`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_activity_creator_id` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_activity_issue_id` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_activity_priority_id` FOREIGN KEY (`priority_id`) REFERENCES `issue_priority` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_activity_project_version_id` FOREIGN KEY (`project_version_id`) REFERENCES `project_version` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_activity_status_id` FOREIGN KEY (`status_id`) REFERENCES `issue_status` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `issue_activity`
+--
+
+LOCK TABLES `issue_activity` WRITE;
+/*!40000 ALTER TABLE `issue_activity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `issue_activity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `issue_priority`
+--
+
+DROP TABLE IF EXISTS `issue_priority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `issue_priority` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `index` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_UNIQUE` (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `issue_priority`
+--
+
+LOCK TABLES `issue_priority` WRITE;
+/*!40000 ALTER TABLE `issue_priority` DISABLE KEYS */;
+/*!40000 ALTER TABLE `issue_priority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `issue_status`
+--
+
+DROP TABLE IF EXISTS `issue_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `issue_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `index` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_UNIQUE` (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `issue_status`
+--
+
+LOCK TABLES `issue_status` WRITE;
+/*!40000 ALTER TABLE `issue_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `issue_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` mediumtext,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project_participant`
+--
+
+DROP TABLE IF EXISTS `project_participant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project_participant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `role` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_idx` (`user_id`),
+  KEY `fk_project_idx` (`project_id`),
+  CONSTRAINT `fk_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_participant`
+--
+
+LOCK TABLES `project_participant` WRITE;
+/*!40000 ALTER TABLE `project_participant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project_participant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project_version`
+--
+
+DROP TABLE IF EXISTS `project_version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project_version` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(225) DEFAULT NULL,
+  `description` mediumtext,
+  `project_id` int(11) DEFAULT NULL,
+  `is_active` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_project_id_idx` (`project_id`),
+  CONSTRAINT `fk_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_version`
+--
+
+LOCK TABLES `project_version` WRITE;
+/*!40000 ALTER TABLE `project_version` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project_version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `date_registration` int(11) DEFAULT NULL,
+  `date_last_login` int(11) DEFAULT NULL,
+  `role` int(2) DEFAULT NULL,
+  `username` varchar(40) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `api_key` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `api_key_UNIQUE` (`api_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-15 13:05:48
